2b789075b98466e701d5e846f87eaa5db3d3f7f4
saltycaramel22 <arav00422@gmail.com>
2024-08-23 20:33:57 -0400

    Almost finished. Can't figure out how to make the boxes inside of the container change size automatically. I have the formula for it, but I don't know how to implement it in CSS.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+I am going to try to create an Etch-A-Sketch.
---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let promptButton = document.getElementById("promptButton");
+    promptButton.textContent = "Grid Size";
+
+    promptButton.addEventListener("click", () => {
+        let userInput = prompt("Please enter a number (up to 100):");
+
+        if (userInput !== null) { 
+            let number = parseFloat(userInput);
+
+            if (!isNaN(number) && (number <= 100)) {
+                alert(`You entered the number: ${number}`);
+                createGrid(number);
+            } else {
+                alert("That's not a valid number.");
+            }
+        }
+    });
+});
+
+
+let container = document.getElementById("container");
+
+function createGrid(size) {
+    for (let rows = 0; rows < size; rows++) {
+        for (let columns = 0; columns < size; columns++) {
+            let box = document.createElement("div");
+            box.className = "box";
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor = "black";
+            });
+            container.appendChild(box);
+        }
+    }
+}
