92ad7c14e8ae40d363adc671a5781e410c9999e2
saltycaramel22 <arav00422@gmail.com>
2024-08-30 16:36:56 -0400

    Finished it.

--- script.js
+++ script.js
@@ -22,14 +22,22 @@ document.addEventListener("DOMContentLoaded", () => {
 let container = document.getElementById("container");
 
 function createGrid(size) {
-    for (let rows = 0; rows < size; rows++) {
-        for (let columns = 0; columns < size; columns++) {
-            let box = document.createElement("div");
-            box.className = "box";
-            box.addEventListener("mouseenter", () => {
-                box.style.backgroundColor = "black";
-            });
-            container.appendChild(box);
-        }
+    container.innerHTML = '';
+
+    let boxSize = 960 / size;
+
+    container.style.gridTemplateColumns = `repeat(${size}, ${boxSize}px)`;
+    container.style.gridTemplateRows = `repeat(${size}, ${boxSize}px)`;
+
+    for (let i = 0; i < size * size; i++) {
+        let box = document.createElement("div");
+        box.className = "box";
+        box.style.width = `${boxSize}px`;
+        box.style.height = `${boxSize}px`;
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "black";
+        });
+        container.appendChild(box);
+        
     }
 }
